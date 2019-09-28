136e470af4aaf6d62d101ed422103c4ca24e4a4b
Jie Lin <linjie997@gmail.com>
2019-09-28 15:28:33 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'linjie997-github-io';
+}
